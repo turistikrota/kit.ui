eef7606138d00f84a15b785fff395863a5c40192
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-08-05 18:09:52 +0300

    component/badge: add the component

---
+++ packages/ui/src/badge/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { FormSize, FullVariant, FullVariantClasses } from '../types'
+
+type Props = {
+  variant?: FullVariant
+  size?: FormSize
+}
+
+const SizeClasses: Record<FormSize, string> = {
+  sm: 'px-2 py-1 text-xs rounded',
+  md: 'px-3 py-1.5 text-sm rounded-md',
+  lg: 'px-4 py-2 text-base rounded-lg',
+}
+
+const Badge: React.FC<React.PropsWithChildren<Props>> = ({ variant = 'default', size = 'md', children }) => {
+  return (
+    <span className={`inline-flex items-center font-semibold ${SizeClasses[size]} ${FullVariantClasses[variant]}`}>
+      {children}
+    </span>
+  )
+}
+
+export default Badge
--- packages/ui/src/types/index.ts
+++ packages/ui/src/types/index.ts
@@ -55,6 +55,21 @@ export type Variant =
   | 'opacity'
   | 'vip'
 
+export type FullVariant =
+  | 'success'
+  | 'warning'
+  | 'danger'
+  | 'primary'
+  | 'default'
+  | 'secondary'
+  | 'yellow'
+  | 'blue'
+  | 'green'
+  | 'purple'
+  | 'orange'
+  | 'indigo'
+  | 'teal'
+
 export const TextSize: Record<Size, string> = {
   xs: 'text-xs',
   sm: 'text-sm',
@@ -64,6 +79,22 @@ export const TextSize: Record<Size, string> = {
   '2xl': 'text-2xl',
   '3xl': 'text-3xl',
   '4xl': 'text-4xl',
+}
+
+export const FullVariantClasses: Record<FullVariant, string> = {
+  success: 'bg-green-100 bg-opacity-50 text-green-500 dark:bg-green-500 dark:bg-opacity-10 dark:text-green-100',
+  warning: 'bg-yellow-100 bg-opacity-50 text-yellow-500 dark:bg-yellow-500 dark:bg-opacity-10 dark:text-yellow-100',
+  danger: 'bg-red-100 bg-opacity-50 text-red-500 dark:bg-red-500 dark:bg-opacity-10 dark:text-red-100',
+  primary: 'bg-blue-100 bg-opacity-50 text-blue-500 dark:bg-blue-500 dark:bg-opacity-10 dark:text-blue-100',
+  default: 'bg-second bg-opacity-50 text-second dark:bg-second dark:bg-opacity-50 dark:text-second',
+  secondary: 'bg-second bg-opacity-50 text-second dark:bg-second dark:bg-opacity-50 dark:text-second',
+  yellow: 'bg-yellow-100 bg-opacity-50 text-yellow-500 dark:bg-yellow-500 dark:bg-opacity-10 dark:text-yellow-100',
+  blue: 'bg-blue-100 bg-opacity-50 text-blue-500 dark:bg-blue-500 dark:bg-opacity-10 dark:text-blue-100',
+  green: 'bg-green-100 bg-opacity-50 text-green-500 dark:bg-green-500 dark:bg-opacity-10 dark:text-green-100',
+  purple: 'bg-purple-100 bg-opacity-50 text-purple-500 dark:bg-purple-500 dark:bg-opacity-10 dark:text-purple-100',
+  orange: 'bg-orange-100 bg-opacity-50 text-orange-500 dark:bg-orange-500 dark:bg-opacity-10 dark:text-orange-100',
+  indigo: 'bg-indigo-100 bg-opacity-50 text-indigo-500 dark:bg-indigo-500 dark:bg-opacity-10 dark:text-indigo-100',
+  teal: 'bg-teal-100 bg-opacity-50 text-teal-500 dark:bg-teal-500 dark:bg-opacity-10 dark:text-teal-100',
 }
 
 export function isSize(size: string): size is Size {
