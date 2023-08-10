401d82b1cfd7f612f565b5219698c5d4d9f61fd7
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-08-11 01:32:24 +0300

    util/deep-merge: add the util, test and docs

--- packages/ui/src/utils/index.ts
+++ packages/ui/src/utils/index.ts
@@ -20,3 +20,29 @@ export const deepEqual = (obj1: any, obj2: any): boolean => {
   }
   return true
 }
+
+type AnyObject = Record<string, any>
+
+export const deepMerge = <T extends AnyObject, S extends AnyObject>(target: T, source: S): T & S => {
+  if (typeof source !== 'object' || source === null) {
+    return source as any
+  }
+
+  const merged: AnyObject = { ...target }
+
+  for (const key in source) {
+    if (Object.prototype.hasOwnProperty.call(source, key)) {
+      if (typeof source[key] === 'object' && source[key] !== null) {
+        if (Array.isArray(source[key])) {
+          merged[key] = [...(merged[key] || []), ...source[key]]
+        } else {
+          merged[key] = deepMerge(merged[key] || {}, source[key])
+        }
+      } else {
+        merged[key] = source[key]
+      }
+    }
+  }
+
+  return merged as T & S
+}
