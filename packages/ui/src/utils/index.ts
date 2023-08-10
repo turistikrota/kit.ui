7d188bc192dd5942495f19cea6e4fd9c4f3844c3
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-08-11 01:43:41 +0300

    util/find-diff: add the findDiff utility

--- packages/ui/src/utils/index.ts
+++ packages/ui/src/utils/index.ts
@@ -46,3 +46,34 @@ export const deepMerge = <T extends AnyObject, S extends AnyObject>(target: T, s
 
   return merged as T & S
 }
+
+type NestedObject = { [key: string]: NestedValue }
+type NestedValue = string | number | boolean | null | NestedObject | NestedValue[]
+
+export function findDiff(obj1: NestedObject, obj2: NestedObject): NestedObject {
+  const diff: NestedObject = {}
+
+  const keys = new Set([...Object.keys(obj1), ...Object.keys(obj2)])
+
+  for (const key of keys) {
+    if (Array.isArray(obj1[key]) && Array.isArray(obj2[key])) {
+      if (JSON.stringify(obj1[key]) !== JSON.stringify(obj2[key])) {
+        diff[key] = obj2[key]
+      }
+    } else if (
+      typeof obj1[key] === 'object' &&
+      obj1[key] !== null &&
+      typeof obj2[key] === 'object' &&
+      obj2[key] !== null
+    ) {
+      const nestedDiff = findDiff(obj1[key] as NestedObject, obj2[key] as NestedObject)
+      if (Object.keys(nestedDiff).length > 0) {
+        diff[key] = nestedDiff
+      }
+    } else if (obj1[key] !== obj2[key]) {
+      diff[key] = obj2[key]
+    }
+  }
+
+  return diff
+}
