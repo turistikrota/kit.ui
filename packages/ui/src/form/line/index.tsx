0cfcbff616c8b0db3575d45be0929102bb787372
9ssi7 <info@ssibrahimbas.com>
2023-12-03 19:58:04 +0300

    feature/line-form: fix responsive

--- packages/ui/src/form/line/index.tsx
+++ packages/ui/src/form/line/index.tsx
@@ -9,24 +9,44 @@ type Props = {
   className?: string
 }
 
-function Left({ children }: React.PropsWithChildren) {
-  return <div className='flex grow flex-col justify-start'>{children}</div>
+function Left({ children, className }: React.PropsWithChildren<Props>) {
+  return <div className={`flex grow flex-col justify-start ${className ? className : ''}`}>{children}</div>
 }
 
-function Title({ children }: React.PropsWithChildren) {
-  return <div className='text-left text-lg font-bold text-gray-800 dark:text-gray-200'>{children}</div>
+function Title({ children, className }: React.PropsWithChildren<Props>) {
+  return (
+    <div
+      className={`text-center text-lg font-bold text-gray-800 dark:text-gray-200 md:text-left ${
+        className ? className : ''
+      }`}
+    >
+      {children}
+    </div>
+  )
 }
 
-function Description({ children }: React.PropsWithChildren) {
-  return <div className='text-left text-sm text-gray-600 dark:text-gray-400'>{children}</div>
+function Description({ children, className }: React.PropsWithChildren<Props>) {
+  return (
+    <div
+      className={`mt-1 text-center text-sm text-gray-600 dark:text-gray-400 md:mt-0 md:text-left ${
+        className ? className : ''
+      }`}
+    >
+      {children}
+    </div>
+  )
 }
 
-function Right({ children }: React.PropsWithChildren) {
-  return <div className='flex justify-end'>{children}</div>
+function Right({ children, className }: React.PropsWithChildren<Props>) {
+  return <div className={`flex min-w-max justify-end ${className ? className : ''}`}>{children}</div>
 }
 
 const LineForm: FormType = ({ children, className }) => {
-  return <div className={`flex items-center justify-between ${className ? className : ''}`}>{children}</div>
+  return (
+    <div className={`flex flex-col items-center justify-between gap-2 md:flex-row ${className ? className : ''}`}>
+      {children}
+    </div>
+  )
 }
 
 Left.Title = Title
