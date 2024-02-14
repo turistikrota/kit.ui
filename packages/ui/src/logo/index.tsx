87d76a765576e473f70ee358a7e7e5b8cdd179e6
9ssi7 <info@ssibrahimbas.com>
2024-02-15 01:50:53 +0300

    feature/logo: add the new version

--- packages/ui/src/logo/index.tsx
+++ packages/ui/src/logo/index.tsx
@@ -1,49 +1,63 @@
 import React from 'react'
+import { FullVariant } from '../types'
 
 type Props = {
   width?: number
   height?: number
   className?: string
+  subApp?: string
+  variant?: FullVariant
+  vertical?: boolean
 }
 
-type BadgeProps = {
-  className?: string
+const floorNumber = 14.915
+const floorHeightNumber = 26.92
+
+const calculateTextLength = (text: string) => {
+  return text.length * floorNumber
 }
 
-type LogoComponent = React.FC<React.PropsWithChildren<Props>> & {
-  Badge: typeof Badge
-  SubModule: typeof SubModule
+const Variants: Record<FullVariant, string> = {
+  blue: 'fill-blue-500',
+  danger: 'fill-red-500',
+  green: 'fill-green-500',
+  indigo: 'fill-indigo-500',
+  orange: 'fill-orange-500',
+  primary: 'fill-primary',
+  purple: 'fill-purple-500',
+  secondary: 'fill-secondary',
+  success: 'fill-success',
+  teal: 'fill-teal-500',
+  warning: 'fill-warning',
+  yellow: 'fill-yellow-500',
+  default: 'fill-emerald-500',
 }
 
-const Badge: React.FC<React.PropsWithChildren<BadgeProps>> = ({ children, className }) => {
-  return (
-    <span className={className ? className : 'absolute -right-8 -top-2'}>
-      <span className='bg-secondary inline-flex items-center justify-center rounded-md px-1 py-1 text-xs leading-none text-black'>
-        {children}
-      </span>
-    </span>
-  )
-}
-
-const SubModule: React.FC<React.PropsWithChildren> = ({ children }) => {
-  return (
-    <span className='bg-secondary dark:bg-secondary-600 inline-flex items-center justify-center rounded-md px-1.5 py-1.5 text-sm leading-none text-black dark:text-white'>
-      {children}
-    </span>
-  )
-}
-
-const Logo: LogoComponent = ({ children, width = 186, height = 30, className }) => {
+const Logo: React.FC<React.PropsWithChildren<Props>> = ({
+  children,
+  vertical = false,
+  height = 30,
+  className,
+  variant = 'default',
+  subApp,
+}) => {
+  const textLength = calculateTextLength(`turistikrota${subApp ? `${subApp}` : ''}`)
+  const fixedHeight = vertical ? height * 2 + floorNumber / 2 : height
   return (
     <div className={`relative ${className ? className : ''}`}>
-      <svg xmlns='http://www.w3.org/2000/svg' viewBox={`0 0 ${width} ${height}`} width={width} height={height}>
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox={`0 0 ${textLength} ${fixedHeight}`}
+        width={textLength}
+        height={fixedHeight}
+      >
         <text
           style={{
             fill: '#f9a31a',
             fontSize: '34px',
             fontFamily: 'Verdana, Verdana',
           }}
-          transform='translate(0 26.92)'
+          transform={`translate(0, ${floorHeightNumber})`}
         >
           <tspan x='0' y='0'>
             turistik
@@ -55,19 +69,32 @@ const Logo: LogoComponent = ({ children, width = 186, height = 30, className }) 
             fontSize: '34px',
             fontFamily: 'Verdana, Verdana',
           }}
-          transform='translate(118.86 27.08)'
+          transform={`translate(${floorNumber * 8}, ${floorHeightNumber})`}
         >
           <tspan x='0' y='0'>
             rota
           </tspan>
         </text>
+        {subApp && (
+          <text
+            style={{
+              fontSize: '28px',
+              fontFamily: 'Verdana, Verdana',
+            }}
+            className={Variants[variant]}
+            transform={`translate(${vertical ? 0 : textLength - calculateTextLength(subApp) + floorNumber / 2}, ${
+              vertical ? 2 * floorHeightNumber + floorNumber / 3 : floorHeightNumber
+            })`}
+          >
+            <tspan x='0' y='0'>
+              {subApp}
+            </tspan>
+          </text>
+        )}
       </svg>
       {children}
     </div>
   )
 }
-
-Logo.Badge = Badge
-Logo.SubModule = SubModule
 
 export default Logo
