c138ba62543438b137e68a8417fd8791e3e2de6a
9ssi7 <info@ssibrahimbas.com>
2024-02-15 03:23:54 +0300

    feature/logo: add modern logo

--- packages/ui/src/logo/index.tsx
+++ packages/ui/src/logo/index.tsx
@@ -10,91 +10,58 @@ type Props = {
   vertical?: boolean
 }
 
-const floorNumber = 14.915
-const floorHeightNumber = 26.92
-const subAppMargin = 9.02
-
-const calculateTextLength = (text: string) => {
-  return text.length * floorNumber
-}
-
 const Variants: Record<FullVariant, string> = {
-  blue: 'fill-blue-500',
-  danger: 'fill-red-500',
-  green: 'fill-green-500',
-  indigo: 'fill-indigo-500',
-  orange: 'fill-orange-500',
-  primary: 'fill-primary',
-  purple: 'fill-purple-500',
-  secondary: 'fill-secondary',
-  success: 'fill-success',
-  teal: 'fill-teal-500',
-  warning: 'fill-warning',
-  yellow: 'fill-yellow-500',
-  default: 'fill-emerald-500',
+  blue: 'text-blue-500',
+  danger: 'text-red-500',
+  green: 'text-green-500',
+  indigo: 'text-indigo-500',
+  orange: 'text-orange-500',
+  primary: 'text-primary',
+  purple: 'text-purple-500',
+  secondary: 'text-secondary',
+  success: 'text-success',
+  teal: 'text-teal-500',
+  warning: 'text-warning',
+  yellow: 'text-yellow-500',
+  default: 'text-emerald-500',
 }
 
 const Logo: React.FC<React.PropsWithChildren<Props>> = ({
-  children,
   vertical = false,
-  height = 30,
   className,
   variant = 'default',
   subApp,
 }) => {
-  const textLength = calculateTextLength(`turistikrota${subApp ? `${subApp}` : ''}`)
-  const fixedHeight = vertical ? height * 2 + floorNumber / 2 : height
   return (
-    <div className={`relative ${className ? className : ''}`}>
-      <svg
-        xmlns='http://www.w3.org/2000/svg'
-        viewBox={`0 0 ${textLength} ${fixedHeight}`}
-        width={textLength}
-        height={fixedHeight}
-        overflow={'initial'}
-      >
+    <div className={`${!vertical ? 'flex' : ''} ${className ? className : ''}`}>
+      <div className='flex'>
         <text
           style={{
-            fill: '#f9a31a',
-            fontSize: '34px',
             fontFamily: 'Verdana, Verdana',
           }}
-          transform={`translate(0, ${floorHeightNumber})`}
+          className='text-secondary text-3xl'
         >
-          <tspan x='0' y='0'>
-            turistik
-          </tspan>
+          turistik
         </text>
         <text
           style={{
-            fill: '#3397e6',
-            fontSize: '34px',
             fontFamily: 'Verdana, Verdana',
           }}
-          transform={`translate(${floorNumber * 8}, ${floorHeightNumber})`}
+          className='text-primary text-3xl'
         >
-          <tspan x='0' y='0'>
-            rota
-          </tspan>
+          rota
         </text>
-        {subApp && (
-          <text
-            style={{
-              fontSize: '30px',
-              fontFamily: 'Verdana, Verdana',
-            }}
-            className={Variants[variant]}
-            transform={`translate(${vertical ? 0 : textLength - calculateTextLength(subApp) + subAppMargin}, ${
-              vertical ? 2 * floorHeightNumber + floorNumber / 3 : floorHeightNumber
-            })`}
-          >
-            <tspan x='0' y='0'>
-              {subApp}
-            </tspan>
-          </text>
-        )}
-      </svg>
-      {children}
+      </div>
+      {subApp && (
+        <text
+          style={{
+            fontFamily: 'Verdana, Verdana',
+          }}
+          className={`${Variants[variant]} text-3xl ${vertical ? 'mx-auto' : ''}`}
+        >
+          {subApp}
+        </text>
+      )}
     </div>
   )
 }
---
+++ packages/ui/src/logo/modern.tsx
@@ -0,0 +1,58 @@
+import React, { FC } from 'react'
+
+type Variant = 'primary' | 'secondary'
+
+type LinkComponent = FC<{ href: string; target?: string; children: React.ReactNode }>
+
+type LogoItem = {
+  text: string
+  variant: Variant
+}
+
+const Variants: Record<Variant, string> = {
+  primary: 'text-primary',
+  secondary: 'text-secondary',
+}
+
+const HoverVariants: Record<Variant, string> = {
+  primary: 'group-hover:text-primary',
+  secondary: 'group-hover:text-secondary',
+}
+
+type LogoProps = {
+  items: LogoItem[]
+  active?: boolean
+  main?: boolean
+  link: string
+  LinkComponent: LinkComponent
+}
+
+const ModernLogo: FC<LogoProps> = ({ items, active, main, link, LinkComponent }) => {
+  return (
+    <LinkComponent href={link}>
+      <span
+        style={{
+          fontFamily: 'Verdana, Verdana',
+        }}
+        className={`group flex rounded-md px-2.5 py-1.5 ${
+          active ? 'bg-primary' : 'hover:bg-primary '
+        } bg-opacity-10 transition-colors duration-200 hover:bg-opacity-10`}
+      >
+        {items.map((item, index) => (
+          <span
+            key={index}
+            className={`${
+              active || main
+                ? Variants[item.variant]
+                : `${HoverVariants[item.variant]} text-gray-800 dark:text-gray-300`
+            } text-lg transition-colors duration-200`}
+          >
+            {item.text}
+          </span>
+        ))}
+      </span>
+    </LinkComponent>
+  )
+}
+
+export default ModernLogo
