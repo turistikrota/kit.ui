010650b4909d8e3b2b7eef0121a7d8f05da4a512
9ssi7 <info@ssibrahimbas.com>
2024-02-16 00:37:20 +0300

    refactor/key-value: add line clamp

--- packages/ui/src/key-value/index.tsx
+++ packages/ui/src/key-value/index.tsx
@@ -3,13 +3,16 @@ import React, { FC } from 'react'
 type Props = {
   label: string
   value: string
+  line?: number
 }
 
-const KeyValue: FC<Props> = ({ label, value }) => {
+const Lines: string[] = ['line-clamp-1', 'line-clamp-2', 'line-clamp-3', 'line-clamp-4', 'line-clamp-5', 'line-clamp-6']
+
+const KeyValue: FC<Props> = ({ label, value, line = 0 }) => {
   return (
     <div className='bg-second flex flex-col rounded-md p-2'>
       <span className='text-xs font-semibold leading-tight text-gray-600 dark:text-gray-400'>{label}</span>
-      <span className='text-md font-semibold'>{value}</span>
+      <span className={`text-md font-semibold ${line < 6 ? Lines[line] : 'line-clamp-6'}`}>{value}</span>
     </div>
   )
 }
